232149ef57607d13bede48f9409a79fa255defe6
ravi <util4ravi@gmail.com>
2020-06-21 02:07:04 +0530

    Test files

---
+++ precache-manifest.e8ac6110cc6e52f1d79dc53741cd4734.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94f2a16a18590e644b08eb6263b40e68",
+    "url": "/index.html"
+  },
+  {
+    "revision": "eb37e3d12ee526cf07be",
+    "url": "/static/css/main.f64d33f0.chunk.css"
+  },
+  {
+    "revision": "8961004a6b589eb17a4c",
+    "url": "/static/js/2.e1a0f1bd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e1a0f1bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eb37e3d12ee526cf07be",
+    "url": "/static/js/main.f4f6431d.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
